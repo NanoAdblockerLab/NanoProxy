132b0a951dae1523ff471149bbeb4e20f3ab304f
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-03 12:02:07 -0600

    add cert gen scripts

---
+++ Violentproxy/Violentcert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# Create a certificate and sign it with a certificate authority
+
+openssl genrsa -out /root/Violentcert.private 2048
+# You can change "localhost" to something that will work for you
+openssl req -new -key /root/Violentcert.private -out /root/Violentcert.csr -subj "/O=Violentproxy/CN=localhost"
+openssl x509 -req -in /root/Violentcert.csr -CA /root/Violentca.cert -CAkey /root/Violentca.pem -CAcreateserial -out /root/Violentcert.cert -days 31
+openssl rsa -in /root/Violentcert.private -pubout -out /root/Violentcert.public
