031c9fc596a77ef3207863856f0cb3b246439bee
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-03 15:31:45 -0600

    Update HTTPS Proxy.js

--- Examples/HTTPS Proxy.js
+++ Examples/HTTPS Proxy.js
@@ -1,7 +1,8 @@
 "use strict";
 
-//Silimar to HTTP Proxy, but it tries to start the proxy server in HTTPS mode
-//Assumes certificates are in C:/Violentcert
+//Silimar to "HTTP Proxy.js", but it tries to start the proxy server in HTTPS mode
+//Assumes certificates are in "C:/Violentcert"
+console.log(`Please make sure certificates are copied to "C:/Violentcert"`);
 
 //Load certificate
 const fs = require("fs");
