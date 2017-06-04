9a86562f1591d1ed9d5eb0c204761f5a76bd772c
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-03 20:34:33 -0600

    eh, looks like we have some fundamental design issue...

--- Tests/test-http.js
+++ Tests/test-http.js
@@ -2,6 +2,6 @@
 
 //Start a simple proxy server, it will inject an inline script that write something to the console to all pages that has <head> tag
 //Not very useful
-require("../Violentproxy/Violentengine").start({unsafe: true});
+require("../Violentproxy/Violentengine").start({ unsafe: true });
 
 console.log("Does it work? Test it out: curl -x localhost:12345 example.com");
--- Violentproxy/Violentengine.js
+++ Violentproxy/Violentengine.js
@@ -26,7 +26,8 @@ const engine = (localReq, localRes) => {
     }
     //Pass through some arguments
     options.headers = localReq.headers;
-    options.agent = localReq.agent;
+    //I think this can cause problem if the user agent decides to keep the connection alive
+    //options.agent = localReq.agent;
     options.auth = localReq.auth;
     //Handle internal request loop
     //As I can't easily find out what is my common name, the first proxied request will backloop internally
@@ -40,7 +41,7 @@ const engine = (localReq, localRes) => {
         localRes.end();
     } else {
         //Patch the request
-        const requestResult = exports.requestPatchingProvider(localReq.headers, localReq.url);
+        const requestResult = exports.requestRequestPatchingProvider(localReq.headers, localReq.url);
         //Further process headers so response from remote server can be parsed
         localReq.headers["accept-encoding"] = "gzip, deflate";
         switch (requestResult.result) {
@@ -124,34 +125,57 @@ const engine = (localReq, localRes) => {
 
 /**
  * Proxy engine for CONNECT (HTTPS) requests.
- * @param {any} localRes
- * @param {any} localSocket
- * @param {any} localHead
+ * @param {IncomingMessage} localReq - 
+ * @param {Socket} localSocket
+ * @param {Buffer} localHead
  */
-const engines = (localRes, localSocket, localHead) => {
-    //TODO: https://newspaint.wordpress.com/2012/11/05/node-js-http-and-https-proxy/
-    //      https://nodejs.org/api/http.html#http_event_connect
+const engines = (localReq, localSocket, localHead) => {
+    //https://newspaint.wordpress.com/2012/11/05/node-js-http-and-https-proxy/
     console.log(`CONNECT request received: ${localReq.url}`);
+    //Parse request
+    let [host, port, ...rest] = localReq.url.split(":"); //Expected to be something like example.com:443
+    if (rest.length > 0 || !port || !host) {
+        localSocket.destroy();
+        return;
+    }
+    port = parseInt(port);
+    if (isNaN(port) || port < 0 || port > 65535) {
+        localSocket.destroy();
+        return;
+    }
+    //Now I need to decide what to do, if I establish a two way socket, then I have no control over what is going though the pipe
+    //because the user agent and the server may agree to keep the connection alive, and I won't know when a message ends.
+    //If I initiate a separate request, process it, and send back to the user agent, then I can't keep the connection alive,
+    //for XMLHttpRequest, this can be very expensive. If the server is using custom software, it may cut me off, it shoudln't be a
+    //problem for "standard" software, but it can slow down the connection by quite a bit.
+    //There is no easy way to determine whether a request is XMLHttpRequest, but maybe I can inject some script to inform me to use
+    //socket for subsequent requests.
+
+    //Connect using socket
+    //const remoteSocket = new net.Socket();
+    //remoteSocket.connect(port, host, () => {
+    //
+    //});
 
 };
 
 /**
- * Process final request result and send it to client.
+ * Process final request result of a REQUEST request and send it to client.
  * @param {http.ServerResponse} localRes - The object that can be used to respond client request.
  * @param {http.IncomingMessage} remoteRes - The object that contains data about server response.
  * @param {string} url - The request URL.
  * @param {string} responseText - The response text.
  */
 const finalize = (localRes, remoteRes, url, responseText) => {
-    const text = exports.responsePatchingProvider(remoteRes.headers, url, responseText);
+    const text = exports.requestResponsePatchingProvider(remoteRes.headers, url, responseText);
     //So I don't need to encode it again
     remoteRes.headers["content-encoding"] = "identity";
     //The length will be changed when it is patched, let the browser figure out how long it actually is
     //I can probably count that, I'll pass in an updated length if removing the header causes problems
     delete remoteRes.headers["content-length"];
-    //TODO: Patch Content Security Policy to allow Userscript callback
+    //TODO: Patch Content Security Policy to allow injected scripts to run
+    //      Maybe this should e done by patching provider
     localRes.writeHead(remoteRes.statusCode, remoteRes.statusMessage, remoteRes.headers);
-    //TODO: Pass better stuff to the patching provider
     localRes.write(text);
     localRes.end();
 };
@@ -198,8 +222,8 @@ exports.requestResult = {
 };
 
 /**
- * Request patching provider.
- * This function can modify a request. Override this function to install your provider.
+ * REQUEST request patching provider.
+ * This function can modify a REQUEST request. Override this function to install your provider.
  * @var {Function}
  * @param {Object} headers - The headers of the request, passed over by reference and changes will be reflected.
  ** "accept-encoding" cannot be changed as it wil causes parsing issues.
@@ -208,7 +232,7 @@ exports.requestResult = {
  ** {Enumeration} result - The request patching result.
  ** {Any} extra - Appropriate extra information for the request patching result.
  */
-exports.requestPatchingProvider = (headers, url) => {
+exports.requestRequestPatchingProvider = (headers, url) => {
     //These parameters are not used
     void headers;
     //This is just an example
@@ -219,8 +243,8 @@ exports.requestPatchingProvider = (headers, url) => {
 };
 
 /**
- * Response patching provider.
- * This function can modify a server response. Override this function to install your provider.
+ * REQUEST response patching provider.
+ * This function can modify a REQUEST response from remote server. Override this function to install your provider.
  * @var {Function}
  * @param {Object} headers - The headers of the response, passed over by reference and changes will be reflected.
  ** "content-encoding" and "content-length" cannot be changed as it wil causes parsing issues.
@@ -230,7 +254,7 @@ exports.requestPatchingProvider = (headers, url) => {
  ** Be aware that if text is defined, and nothing is returned, then client will recive an empty body.
  * @return {string} The patched response text.
  */
-exports.responsePatchingProvider = (headers, url, text) => {
+exports.requestResponsePatchingProvider = (headers, url, text) => {
     //These parameters are not used
     void headers;
     void url;
