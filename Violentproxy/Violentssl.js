a22ab3f81375f77fa2904a2302861ddee60be407
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-05 12:32:45 -0600

    step 1

--- Violentproxy/Violentssl.js
+++ Violentproxy/Violentssl.js
@@ -1,7 +1,76 @@
 "use strict";
 
-//I will use Forge, as spawning a child process just to sign a certificate might not be faster.
+//I will use node-forge, as spawning a child-process just to sign a certificate might not be faster.
 //Calling OpenSSL through Bash on Ubuntu on Windows fells like too big an overhead to me.
 //The newest version of OpenSSL in Bash on Ubuntu on Windows also has a few CVE vulnerabilities.
-//I have to worry about cross platform compatibility too, if I decide to use OpenSSL.
-const forge = require("node-forge");
+//That can be manually resolved, but it's too much work to get too little done, and most people
+//won't be confortable working in a terminal.
+const {pki, md} = require("node-forge"),
+    fs = require("fs");
+
+//The certificate and private key of the certificate authority
+let CAcert, CAkey;
+//Cache of available 
+let certCache = {};
+
+/**
+ * Certificate authority attributes.
+ * https://stackoverflow.com/questions/6464129/certificate-subject-x-509
+ * @const {Object}
+ */
+const CAattr = [
+    {
+        shortName: "C", //Country
+        value: "World",
+    },
+    {
+        shortName: "O", //Organization, company, etc.
+        value: "Violentproxy",
+    },
+    {
+        shortName: "OU", //Organizational Unit, like department
+        value: "Violentssl Engine",
+    },
+    {
+        shortName: "ST", //State, province, etc.
+        value: "World",
+    },
+    {
+        shortName: "CN", //Domain, IP, etc. For certificate authority, it's different, it can be anything
+        value: "ViolentCA",
+    },
+    //Optional
+    {
+        shortName: "L", //City, town, etc.
+        value: "World",
+    },
+];
+/**
+ * Certificate authority extensions.
+ * https://access.redhat.com/documentation/en-US/Red_Hat_Certificate_System/8.0/html/Admin_Guide/Standard_X.509_v3_Certificate_Extensions.html
+ * @const {Object}
+ */
+const CAext = [
+    {
+
+    },
+];
+
+/**
+ * Initialize certificate authority, calling sign without calling this function first
+ * will cause problems. This function will throw if the certificate authority could
+ * not be initialized.
+ * @function
+ */
+exports.init = () => {
+
+};
+
+/**
+ * Get a certificate for the current domain, do not pass in domain with wildcard.
+ * @function
+ * @return {Object} An object that can be passed to https.createServer().
+ */
+exports.sign = (domain) => {
+
+};
