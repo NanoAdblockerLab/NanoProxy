31e6e525f8526f206eb90d69626be73306cdb984
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-04 21:34:03 -0600

    Update Violentcert.sh

--- Violentproxy/Violentssl/Violentcert.sh
+++ Violentproxy/Violentssl/Violentcert.sh
@@ -6,6 +6,7 @@ openssl genrsa -out /root/Violentcert.private 2048
 # You can change "localhost" to something that will work for you
 # TODO: https://www.chromestatus.com/feature/4981025180483584
 #       http://blog.endpoint.com/2014/10/openssl-csr-with-alternative-names-one.html
+#       https://stackoverflow.com/questions/21297853/how-to-determine-ssl-cert-expiration-date-from-a-pem-encoded-certificate
 openssl req -new -key /root/Violentcert.private -out /root/Violentcert.csr -subj "/O=Violentproxy/CN=localhost"
 openssl x509 -req -in /root/Violentcert.csr -CA /root/Violentca.crt -CAkey /root/Violentca.pem -CAcreateserial -out /root/Violentcert.crt -days 31
 openssl rsa -in /root/Violentcert.private -pubout -out /root/Violentcert.public
